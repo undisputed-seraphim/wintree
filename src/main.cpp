9620222d2697a7f57a26e96caa148d0f35781cb5
Tan Li Boon <undisputed.seraphim@gmail.com>
2022-07-16 15:00:15 +0900

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <array>
+﻿#include <array>
 #include <cstring>
 #include <string_view>
 #include <vector>
@@ -7,13 +7,15 @@
 #include <Windows.h>
 #include <fileapi.h>
 #include <sys/stat.h>
-#pragma comment(lib, "User32.lib")
+//#pragma comment(lib, "User32.lib")
 
-using namespace std::string_literals;
+using namespace std::literals::string_view_literals;
 
-constexpr std::wstring_view junction(L"├── "), bar(L"│   "), angle(L"└── "), space(L"    ");
+constexpr std::wstring_view junction(L"|-- "), bar(L"|   "), angle(L"\\-- "), space(L"    ");
+//constexpr std::wstring_view junction(L"├── "), bar(L"│   "), angle(L"└── "), space(L"    ");
 
-std::vector<wchar_t> indent;
+int Walk(std::wstring& rootPath, std::vector<std::wstring_view>& headers);
+int Walk(WIN32_FIND_DATAW thisEntry, std::wstring& thisParentPath, std::vector<std::wstring_view>& headers, bool isLast);
 
 void LogSystemError(unsigned long error) {
 	std::array<wchar_t, 1024> buffer; // Stack allocated buffer
@@ -33,80 +35,83 @@ void LogSystemError() {
 	LogSystemError(error);
 }
 
-int GetTabStopWidth() {
-    int tab_stop_width = -1;
-    HANDLE h = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-    if (h != INVALID_HANDLE_VALUE) {
-        DWORD n;
-        if (WriteConsoleW(h, L"\r\t", 2, &n, NULL)) {
-            CONSOLE_SCREEN_BUFFER_INFO info;
-            if (GetConsoleScreenBufferInfo(h, &info))
-                tab_stop_width = info.dwCursorPosition.X;
-        }
-        CloseHandle(h);
-    }
-    return tab_stop_width;
-}
-
-void WCharToUTF8String(const wchar_t* widestr, const unsigned long widestrsize, char* utfstr, const unsigned long utfstrsize) {
-	int defaultCharUsed = false;
-	unsigned long size = WideCharToMultiByte(CP_ACP, WC_COMPOSITECHECK, widestr, widestrsize, NULL, 0, NULL, &defaultCharUsed);
-	size = (size < utfstrsize) ? size : utfstrsize;
-	if (size == 0) {
-		LogSystemError();
-		return;
+int Walk(WIN32_FIND_DATAW thisEntry, std::wstring& thisParentPath, std::vector<std::wstring_view>& headers, bool isLast) {
+	if (L"."sv == thisEntry.cFileName) {
+		return 0;
 	}
-	WideCharToMultiByte(CP_ACP, WC_COMPOSITECHECK, widestr, widestrsize, utfstr, size, NULL, &defaultCharUsed);
+	if (L".."sv == thisEntry.cFileName) {
+		return 0;
+	}
+	for (const auto& entry : headers) {
+		fwprintf(stdout, L"%s", entry.data());
+	}
+	fwprintf_s(stdout, L"%s\n", thisEntry.cFileName);
+	if (thisEntry.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+		headers.back() = (isLast) ? space : bar;
+		const size_t pathLen = thisParentPath.size();
+		thisParentPath.append(thisEntry.cFileName);
+		Walk(thisParentPath, headers);
+		thisParentPath.resize(pathLen);
+		headers.back() = junction;
+	} else {
+		headers.back() = junction;
+	}
+	return 0;
 }
 
-int WalkDirectory(std::wstring current_path) {
-	current_path.append(L"\\*");
-	//fwprintf_s(stderr, L"Walking %s\n", current_path.c_str());
+int Walk(std::wstring& rootPath, std::vector<std::wstring_view>& headers) {
+	rootPath.append((rootPath.back() == L'\\') ? L"*" : L"\\*");
 	WIN32_FIND_DATAW current = {};
-	HANDLE handle = FindFirstFileW(current_path.data(), &current);
+	HANDLE handle = FindFirstFileW(rootPath.data(), &current);
 	if (handle == INVALID_HANDLE_VALUE) {
 		LogSystemError();
 		return 1;
 	}
-	// Flush out '.' and '..' directories.
-	FindNextFileW(handle, &current);
 
-	for (int valid = FindNextFileW(handle, &current); valid != 0; valid = FindNextFileW(handle, &current)) {
-		if (current.cFileName == L"..") {
-			continue;
+	rootPath.pop_back(); // Remove '*'
+	headers.push_back(junction);
+	WIN32_FIND_DATAW recurseEntry = {}, recurseEntryPrev = {};
+	FindNextFileW(handle, &recurseEntryPrev);
+	bool hasNext = true;
+	do {
+		bool isLast = (FindNextFileW(handle, &recurseEntry) == 0);
+		if (isLast) {
+			hasNext = false;
+			headers.back() = angle;
 		}
-		fwprintf_s(stderr, L"%s%s\n", indent.data(), current.cFileName);
-		if (current.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
-			std::wstring enter_path = current_path;
-			enter_path.pop_back();
-			enter_path.append(current.cFileName);
-			indent.back() = L'\t';
-			indent.push_back(L'\0');
-			WalkDirectory(std::move(enter_path));
-			indent.pop_back();
-			indent.back() = L'\0';
-		}
-	}
-	auto error = GetLastError();
-	if (error != ERROR_NO_MORE_FILES) {
-		LogSystemError(error);
-		return 1;
-	}
-	FindClose(handle);
+		Walk(std::move(recurseEntryPrev), rootPath, headers, isLast);
+		recurseEntryPrev = recurseEntry;
+	} while (hasNext);
+	headers.pop_back();
 	return 0;
 }
 
 int wmain(int argc, wchar_t *argv[]) {
-	indent = std::vector<wchar_t>();
-	indent.push_back(L'\0');
-	std::wstring root(1024, '\0');
-	unsigned long len = GetCurrentDirectoryW((unsigned long)root.size(), root.data());
-	if (len == 0) {
-		LogSystemError();
-		return 1;
+	std::wstring root;
+	bool showHidden = false;
+	if (argc > 1) {
+		for (int i = 1; i < argc; ++i) {
+			const wchar_t* arg = argv[i];
+			if (L"-a"sv == arg || L"/a"sv == arg) {
+				showHidden = true;
+			}
+		}
+		// Final entry of args should always be the query directory.
+		root.assign(argv[argc-1], ::wcslen(argv[argc-1]));
+	} else {
+		root.resize(512, '\0');
+		unsigned long len = GetCurrentDirectoryW((unsigned long)root.size(), root.data());
+		if (len == 0) {
+			LogSystemError();
+			return 1;
+		}
+		root.resize(len);
 	}
-	root.resize(len);
-	WalkDirectory(std::move(root));
+	root.reserve(root.size() * 2);
+
+	fwprintf_s(stdout, L"%s\n", root.c_str());
+	std::vector<std::wstring_view> headers;
+	Walk(std::move(root), headers);
 
 	return 0;
 }
